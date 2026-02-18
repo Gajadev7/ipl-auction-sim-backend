989b89b03d120d43fe6861ec2127dce55d3b64fd
gajadev.s <gajadev.s@zohocorp.com>
2026-02-18 16:30:51 +0530

    Swagger

--- src/AuctionServer/Program.cs
+++ src/AuctionServer/Program.cs
@@ -4,12 +4,20 @@ using AuctionServer.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<AuctionManager>();
 builder.Services.AddSingleton<AuctionEventStream>();
 builder.Services.AddSingleton<AiAuctionCoordinator>();
 builder.Services.AddHostedService(serviceProvider => serviceProvider.GetRequiredService<AiAuctionCoordinator>());
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
