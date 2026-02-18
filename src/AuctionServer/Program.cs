4375bfe7a46fccbd0922a311f5bd5614f61a3f77
gajadev.s <gajadev.s@zohocorp.com>
2026-02-18 16:56:46 +0530

    Swagger

--- src/AuctionServer/Program.cs
+++ src/AuctionServer/Program.cs
@@ -13,8 +13,17 @@ builder.Services.AddHostedService(serviceProvider => serviceProvider.GetRequired
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwagger(swaggerOptions =>
+{
+    swaggerOptions.RouteTemplate = "swagger/{documentName}/swagger.json";
+});
+app.UseSwaggerUI(swaggerUiOptions =>
+{
+    swaggerUiOptions.RoutePrefix = "swagger";
+    swaggerUiOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "AuctionServer API v1");
+});
+
+app.MapGet("/swagger", () => Results.Redirect("/swagger/index.html"));
 
 app.MapControllers();
 
